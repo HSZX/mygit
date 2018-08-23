1389d41964ce9ed435b77c670011033097c580c2
HSZX <40757831+HSZX@users.noreply.github.com>
2018-08-23 21:24:26 +0800

    Delete ProfessionService.java

--- heshuang/src/main/java/com/tiles/service/ProfessionService.java
+++
@@ -1,9 +0,0 @@
-package com.tiles.service;
-
-import com.tiles.pojo.Profession;
-
-import java.util.List;
-
-public interface ProfessionService {
-    List<Profession> getAllProfession()throws Exception;
-}
