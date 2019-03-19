cc8fa9476a51b5b22f40e4526bf5507c637b8d81
HSZX <40757831+HSZX@users.noreply.github.com>
2019-03-19 18:46:48 +0800

    Update OSSClientConstants.java

--- heshuang/task8/task8-client/src/main/java/com/task5/until/OSSClientConstants.java
+++ heshuang/task8/task8-client/src/main/java/com/task5/until/OSSClientConstants.java
@@ -2,11 +2,11 @@ package com.task5.until;
 
 public class OSSClientConstants {
     //阿里云API的外网域名
-    public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    public static final String ENDPOINT = "oss-cn-..com";
     //阿里云API的密钥Access Key ID
-    public static final String ACCESS_KEY_ID = "LTAIfK258Gqeppq0";
+    public static final String ACCESS_KEY_ID = "";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESS_KEY_SECRET = "6huGfH0mNH4XJcb0rTMhhWBhSR6hlQ";
+    public static final String ACCESS_KEY_SECRET = "";
     //阿里云API的bucket名称
     public static final String BACKET_NAME = "hszx0214";
     //阿里云API的文件夹名称
